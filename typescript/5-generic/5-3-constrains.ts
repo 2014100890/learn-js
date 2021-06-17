6f50cf24562f327ced421eb9f61bc89ee0faacec
2014100890 <2014100890@khu.ac.kr>
2021-06-17 22:10:19 +0900

    practice OOP and Generic

---
+++ typescript/5-generic/5-3-constrains.ts
@@ -0,0 +1,45 @@
+{
+  interface Employee {
+    pay(): void
+  }
+
+  class FullTimeEmployee implements Employee {
+    pay() {
+      console.log('full time')
+    }
+    workFullTime() {}
+  }
+  class PartTimeEmployee implements Employee {
+    pay() {
+      console.log('part time')
+    }
+    workFullTime() {}
+  }
+
+  function pay<T extends Employee>(employee: T): T {
+    // pay 함수가 있는지 없는지 알 수 없음
+    employee.pay()
+    return employee
+  }
+
+  const neo = new FullTimeEmployee()
+  const bob = new PartTimeEmployee()
+
+  const neoAfterPay = pay(neo) // Employee로 리턴되버림
+  const bobAfterPay = pay(bob) // Employee로 리턴되버림
+
+  const obj1 = {
+    name: 'tomato',
+    age: 6,
+  }
+  const obj2 = {
+    animal: '🐸',
+  }
+
+  function getValue<E>(obj: object, field: string): E {
+    return obj[field]
+  }
+  console.log(getValue(obj1, 'name')) // tomato
+  console.log(getValue(obj1, 'age')) // 6
+  console.log(getValue(obj2, 'animal')) // 6
+}
